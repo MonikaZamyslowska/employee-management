54567d4ba9471bbdb90772a40874d4d09e0a27be
Monika Zamyslowska <zamyslowska.monika@gmail.com>
2022-03-27 21:01:23 +0200

    added keycloak configuration

--- src/main/java/wsb/employeemanagement/employee/repository/EmployeeRepository.java
+++ src/main/java/wsb/employeemanagement/employee/repository/EmployeeRepository.java
@@ -6,7 +6,6 @@ import wsb.employeemanagement.employee.domain.Employee;
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    Employee findByUsername(String username);
     List<Employee> findBySupervisor(Employee employee);
     void removeById(long employeeId);
 }
